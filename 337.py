ce6b75288f32606a31c562c4bd22100feec7ce48
cc712 <chenchen@ysbang.cn>
2019-02-14 10:13:12 +0800

    store

--- 337.py
+++ 337.py
@@ -14,7 +14,35 @@ class Solution:
         :type root: TreeNode
         :rtype: int
         """
-        
+        if root == None or root.val == None:
+                return 0
+        ans = max(self.recursive(root, root.val, True),
+                  self.recursive(root, 0, False))
+        print('ans', ans)
+        return ans
+
+    def recursive(self, node, ans, lastChoosen):
+        # print(ans,lastChoosen)
+        if node == None or node.val == None:
+            return 0
+        # print(node.val)
+        getThis = ans
+        noGetThis = ans
+        v = int(node.val) or 0
+        l = node.left
+        r = node.right
+        if lastChoosen:
+            getThis = ans + \
+                self.recursive(l, ans, False) + \
+                self.recursive(r, ans, False)
+        else:
+            p1 = self.rob(l)
+            p2 = self.rob(r)
+            print(p1, p2, ans)
+            noGetThis = ans + max(p1, p2)
+        a = max(getThis, noGetThis)
+        print('mid:',a)
+        return a
 
 
 s = Solution()
